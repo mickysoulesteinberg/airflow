a5c706f36f82795a29226831147e45dcf968aefd
mickysoulesteinberg <mickysteinberg@gmail.com>
2025-10-23 12:38:30 -0700

    Refactor top_movie_credits DAG: modularize setup and API call tasks, enhance data flow, and improve staging table handling

--- dags/tmdb/top_movie_credits.py
+++ dags/tmdb/top_movie_credits.py
@@ -14,62 +14,92 @@ logger = get_logger(__name__)
 def top_movie_credits():
     api_config = TMDB_DISCOVER_MOVIES
 
-    path_setup = ingestion_tasks.setup_api_path(config=api_config)
+    @task_group
+    def setup(config):
+        path_setup = ingestion_tasks.setup_api_path(config=config)
+        bq_setup = transform_tasks.setup_for_bq(config)
 
-    api = path_setup['api']
-    api_path = path_setup['api_path']
-    gcs_prefix = path_setup['gcs_prefix']
-
-    return_data = {'movie_ids': 'results[].id'}
-
-    task_builder = ingestion_tasks.setup_api_call(config=api_config, year=2020)
-    api_args = task_builder['api_args']
-    gcs_file_name = task_builder['gcs_file_name']
-    call_metadata = task_builder['metadata']
-
-    fetched = ingestion_tasks.api_fetch_and_load(api=api, api_path=api_path,
-                                                 api_args=api_args,
-                                                 gcs_prefix=gcs_prefix,
-                                                 gcs_file_name=gcs_file_name,
-                                                 return_data=return_data,
-                                                 metadata=call_metadata)
+        return {
+            'api': path_setup['api'],
+            'api_path': path_setup['api_path'],
+            'gcs_prefix': path_setup['gcs_prefix'],
+            'table_config': bq_setup['table_config'],
+            'schema_config': bq_setup['schema_config'],
+            'bigquery_dataset': bq_setup['dataset'],
+            'api_root': bq_setup['api_root'],
+            # 'source_type': bq_setup['source_type'],
+            # 'merge_cols': bq_setup['merge_cols']
+        }
     
-    loaded_gcs_path = fetched['gcs_path']
-
-    bq_setup = transform_tasks.setup_for_bq(api_config)
-    table_config = bq_setup['table_config']
-    bq_schema_config = bq_setup['schema_config']
-    bigquery_dataset = bq_setup['dataset']
-    api_root = bq_setup['api_root']
-    source_type = bq_setup['source_type']
-    merge_cols=table_config['row_id']
-
-    transformed_uris = transform_tasks.gcs_transform_for_bq(loaded_gcs_path,
-                                                       table_config=table_config,
-                                                       api_root=api_root)
-
-    bigquery_table_name = 'top_grossing_movies'
-
-    created_staging_table = loader_tasks.create_staging_table(
-        schema_config=bq_schema_config,
-        final_table=bigquery_table_name,
-        dataset=bigquery_dataset
-    )
-
-    loaded_staging_table = loader_tasks.gcs_to_bq_stg(transformed_uris,
-                                                      created_staging_table)
     
-    merged_final_table = loader_tasks.bq_stg_to_final_merge(
-        schema_config=bq_schema_config,
-        staging_table=loaded_staging_table,
-        final_table=bigquery_table_name,
-        dataset=bigquery_dataset,
-        table_config=table_config
-    )
+    @task_group
+    def api_call_iterate(config, api, api_path, gcs_prefix, table_config, api_root, **kwargs):
+        call_builder = ingestion_tasks.setup_api_call(config=config, **kwargs)
 
-    cleanup_tasks.delete_bq_staging_table(
-        loaded_staging_table,
-        wait_for=merged_final_table
-    )
+        api_args = call_builder['api_args']
+        gcs_file_name = call_builder['gcs_file_name']
+        call_metadata = call_builder['metadata']
+
+        fetched = ingestion_tasks.api_fetch_and_load(api=api, api_path=api_path,
+                                                     api_args=api_args,
+                                                     gcs_prefix=gcs_prefix,
+                                                     gcs_file_name=gcs_file_name,
+                                                    #  return_data=return_data,
+                                                     metadata=call_metadata)
+
+        loaded_gcs_path = fetched['gcs_path']
+
+        transformed_uri = transform_tasks.gcs_transform_for_bq(loaded_gcs_path,
+                                                                table_config=table_config,
+                                                                api_root=api_root)
+
+        fetched >> transformed_uri
+
+        return transformed_uri
+
+
+    @task_group
+    def etl_workflow(config, bigquery_table_name, **kwargs):
+        initial_setup = setup(config)
+
+        table_config = initial_setup['table_config']
+        bq_schema_config = initial_setup['schema_config']
+        bigquery_dataset = initial_setup['bigquery_dataset']
+        api_root = initial_setup['api_root']
+        api = initial_setup['api']
+        api_path = initial_setup['api_path']
+        gcs_prefix = initial_setup['gcs_prefix']
+
+        iterate_calls = api_call_iterate(config, api=api, api_path=api_path,
+                                         gcs_prefix=gcs_prefix,
+                                         table_config=table_config, api_root=api_root,
+                                         **kwargs)
+        
+        transformed_uris = iterate_calls
+        
+        created_staging_table = loader_tasks.create_staging_table(
+            schema_config=bq_schema_config,
+            final_table=bigquery_table_name,
+            dataset=bigquery_dataset
+        )
+
+        loaded_staging_table = loader_tasks.gcs_to_bq_stg(transformed_uris, created_staging_table)
+
+        merged_final_table = loader_tasks.bq_stg_to_final_merge(
+            schema_config=bq_schema_config,
+            staging_table=loaded_staging_table,
+            final_table=bigquery_table_name,
+            dataset=bigquery_dataset,
+            table_config=table_config
+        )
+
+        cleanup_tasks.delete_bq_staging_table(
+            loaded_staging_table,
+            wait_for=merged_final_table
+        )
+
+
+    movies = etl_workflow(TMDB_DISCOVER_MOVIES, 'top_grossing_movies', year=2020)
+    
 
 top_movie_credits()
